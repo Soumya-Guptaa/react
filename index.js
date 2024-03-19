8bcc5a4418ead3f8ccb5f50eb8927726e4a8e856
Soumya Gupta <soumyagupta@Soumyas-MacBook-Air-2.local>
2024-03-19 16:17:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1" , {id : "heading"} , "this is my heelo");
+        const root = ReactDOM.createRoot(document.getElementById("root")) ;
+        root.render(heading);
